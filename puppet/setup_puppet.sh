f1c908a5cff2bcb2ef24770df346c224f6a05a0f
Ubuntu <ubuntu@ip-10-224-115-200.eu-west-1.compute.internal>
2012-10-16 19:42:51 +0000

    Improve setup_puppet.sh
    Copy manifests into the puppet folder
    do apt-get install non-interactively

--- puppet/setup_puppet.sh
+++ puppet/setup_puppet.sh
@@ -7,13 +7,15 @@ _EOF_
 
 apt-get update
 
-apt-get install puppet-common
+apt-get install -y puppet-common
 
 git clone https://github.com/puppetlabs/puppetlabs-vcsrepo.git /etc/puppet/modules/puppetlabs-vcsrepo
 git clone https://github.com/rcrowley/puppet-pip.git /etc/puppet/modules/puppet-pip.git
 
 puppet module install puppetlabs-stdlib
 puppet module install akumria-postgresql
+
+cp -r manifests/openerp /etc/puppet/manifests/
 
 cd /etc/puppet/modules/puppetlabs-vcsrepo/
 patch -p1 < /home/ubuntu/miscellaneous/vcsrepo/git.patch
